f92e679df8ac499241057b47f0ffc52952b86ff3
CyberCherney <CyberSecCherney@proton.me>
2023-05-01 21:09:45 -0500

    added automator.js comments

--- bitburner/intro_scripts/automator.js
+++ bitburner/intro_scripts/automator.js
@@ -1,3 +1,11 @@
+/** 
+ * this script is effectively an 'easy afk hack everything' script
+ * no doubt it needs improvement to minmax but I'm not sure how far bitburner goes
+ * in short I have a hacking script named 'ezhack.js' which is placed on machines crawler.js roots
+ * then it is run with the max amount of threads and fed two files: action.txt and host.txt
+ * action determines if it will hack or weaken, host determines the target
+ * commandcenter.js controls what is in those files and placing them on rooted machines
+*/
 
 /** @param {NS} ns */
 export async function main(ns) {
