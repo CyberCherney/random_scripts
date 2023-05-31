2c247bf60e2d92f84f50d183a51a860ec56e3f83
CyberCherney <CyberSecCherney@proton.me>
2023-05-30 19:28:25 -0500

    added prompts and options

--- bitburner/BitNode9/hacknetrun.js
+++ bitburner/BitNode9/hacknetrun.js
@@ -4,9 +4,29 @@ import { hacknetServers, runScript } from 'basic.js';
 // for running a script on all bought hacknet servers
 export async function main(ns) {
 	ns.tail();
-	var servers = hacknetServers(ns);
-	ns.print(servers)
-	for (let i=0; i < servers[i].length; i++) {
-		runScript(ns, servers[i], 'weaken.js', 'max', true);
+	ns.clearLog();
+	const command = await ns.prompt("Select the desired mode: ", {
+		type: "select",
+		choices: ["force", "start", "kill"]
+	});
+	const servers = hacknetServers(ns);
+	ns.print(servers);
+
+	if (command == "force" || command == "kill") {
+		ns.print('killing processes');
+		for (let i = 0; i < servers.length; i++) {
+			ns.killall(servers[i])
+		}
 	}
+
+	if (command == "force" || command == "start") {
+		var script = await ns.prompt('What script should be run?', {type: 'text'});
+		if (!script) {script = 'grow.js'}
+		ns.print('starting script: ' + script);
+		for (let i = 0; i < servers.length; i++) {
+			runScript(ns, servers[i], script, 'max', false);
+		}
+	}
+
+
 }
