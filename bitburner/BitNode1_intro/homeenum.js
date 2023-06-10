c29e0038a42e9384c87ee2e236c66f0f272bb1e7
CyberCherney <CyberSecCherney@proton.me>
2023-06-10 17:20:52 -0500

    modified process spawning

--- bitburner/BitNode1_intro/homeenum.js
+++ bitburner/BitNode1_intro/homeenum.js
@@ -3,20 +3,22 @@
 import { scan } from "basic.js";
 
 export async function main(ns) {
+	//ns.tail();
 	const servers = scan(ns);
 
 	var ram = ns.getServerMaxRam('home');
 	var neededRam = ns.getScriptRam('homehack.js');
-	var threads = Math.floor((ram - 20)/neededRam); 
+	var threads = Math.floor((ram - 40)/neededRam); 
 	for (let i = 0; i < servers.length; i++) {
 		if (ns.hasRootAccess(servers[i])) {
 			var cash = ns.getServerMoneyAvailable(servers[i]);
 			var max = ns.getServerMaxMoney(servers[i]);
-			if ((cash/max) > .3 && servers[i] != ns.read('host.txt')) {
+			if ((cash/max) > .3 && servers[i] != ns.read('target.txt')) {
 				ns.spawn('homehack.js', threads, servers[i]);
 			} else {
-				var host = ns.read('host.txt');
-				ns.spawn('homehack.js', threads, host);
+				var file = ns.read('target.txt');
+				var json = JSON.parse(file);
+				ns.spawn('homehack.js', threads, json.host);
 			}
 		}
 	}
