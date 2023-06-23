191466d855d34eec7229a2c1860fd9fbf0feb6f8
CyberCherney <CyberSecCherney@proton.me>
2023-06-22 22:15:03 -0500

    added breaks and json file read

--- bitburner/BitNode1_intro/homeenum.js
+++ bitburner/BitNode1_intro/homeenum.js
@@ -3,22 +3,24 @@
 import { scan } from "basic.js";
 
 export async function main(ns) {
-	//ns.tail();
+	ns.tail();
 	const servers = scan(ns);
 
 	var ram = ns.getServerMaxRam('home');
 	var neededRam = ns.getScriptRam('homehack.js');
-	var threads = Math.floor((ram - 40)/neededRam); 
+	var threads = Math.floor((ram - 25)/neededRam); 
 	for (let i = 0; i < servers.length; i++) {
 		if (ns.hasRootAccess(servers[i])) {
 			var cash = ns.getServerMoneyAvailable(servers[i]);
 			var max = ns.getServerMaxMoney(servers[i]);
 			if ((cash/max) > .3 && servers[i] != ns.read('target.txt')) {
-				ns.spawn('homehack.js', threads, servers[i]);
+				await ns.run('homehack.js', threads, servers[i]);
+				break
 			} else {
 				var file = ns.read('target.txt');
 				var json = JSON.parse(file);
-				ns.spawn('homehack.js', threads, json.host);
+				await ns.run('homehack.js', threads, json.host);
+				break
 			}
 		}
 	}
