f36d9a4c56b5f337dfaa43b493bae73f0c6273f2
CyberCherney <CyberSecCherney@proton.me>
2023-06-26 00:01:54 -0500

    added initial json overwrite

--- bitburner/BitNode1_intro/start.js
+++ bitburner/BitNode1_intro/start.js
@@ -2,8 +2,20 @@
 // basic script to initialize text documents and start automator.js
 export async function main(ns) {
 	await ns.write('ram.txt', '1', 'w');
-	await ns.write('host.txt', 'n00dles', 'w');
+
+	if (ns.fileExists('target.txt')) {
+			//ns.tail();
+			const file = ns.read('target.txt');
+			const json = JSON.parse(file);
+			var json.host = 'n00dles';
+			var target = JSON.stringify(json);
+			await ns.write('target.txt', target, 'w');
+	} else {
+		var target = { 'host': 'n00dles', 'action': 'weaken', 'endHost': 'ecorp' }
+		var json = JSON.stringify(json);
+		await ns.write('target.txt', json, 'w');
+	}
+
+	
 	ns.run('automator.js');
-	ns.asleep(10000);
-	await ns.run('homeenum.js');
 }
