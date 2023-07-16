a3b2548ebc83620a432ea695b5e3683632571188
CyberCherney <CyberSecCherney@proton.me>
2023-07-16 00:23:32 -0500

    integrated basic.js functions

--- bitburner/BitNode1_intro/scriptall.js
+++ bitburner/BitNode1_intro/scriptall.js
@@ -1,30 +1,40 @@
 /** @param {NS} ns */
+
+import { scan, hacknetServers, runScript } from "basic.js";
+
+
 // finds all servers, transfers a script, then runs it
 // used when I need rep/xp
 export async function main(ns) {
 	ns.tail();
-	var script = ns.prompt('Enter the script to run: ', {type: 'text'});
+	ns.clearLog();
+	ns.disableLog('scan');
+	ns.disableLog('scp');
+	ns.disableLog('exec');
+	ns.disableLog('sleep');
+	ns.disableLog('getServerMaxRam');
+	ns.disableLog('getServerUsedRam');
+	ns.disableLog('killall');
+	var script = await ns.prompt('Enter the script to run: ', {type: 'text'});
 	if (!script) {var script = 'share.js'}
-	var allhosts = ns.scan();
-	for (let i = 0; i < allhosts.length; i++) {
-		var iter = ns.scan(allhosts[i]);
-		for (let k=0; k < iter.length; k++) {
-			if (allhosts.indexOf(iter[k]) == -1) {
-				ns.print(iter[k]);
-				allhosts.push(iter[k]);
 
-			}
-		}
+	var hosts = scan(ns);
+	var hacknetHosts = hacknetServers(ns);
+	var ownedHosts = ns.getPurchasedServers();
+	
+	
+	for (let i = 0; i < hosts.length; i++) {
+		runScript(ns, hosts[i], script, 'max', true);
 	}
-	for (let i = 0; i < allhosts.length; i++) {
-		ns.scp(script, allhosts[i]);
-		var max = ns.getServerMaxRam(allhosts[i]);
-		var used = ns.getServerUsedRam(allhosts[i]);
-		var ram = max - used;
-		var scriptRam = ns.getScriptRam(script);
-		if (ram >= scriptRam) {
-			var threads = Math.floor(ram / scriptRam);
-			ns.exec(script, allhosts[i], threads);
-		}
+
+	for (let i = 0; i < hacknetHosts.length; i++) {
+		runScript(ns, hacknetHosts[i], script, 'max', true);
 	}
+
+	for (let i = 0; i < ownedHosts.length; i++) {
+		runScript(ns, ownedHosts[i], script, 'max', true);
+	}	
+
+	// function runScript(ns, host, script, threadCount, forceRerun)
+
 }
