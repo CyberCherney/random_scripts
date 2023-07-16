2372b6d99b58a6c11f102caa9c810b0a9ed9bde4
CyberCherney <CyberSecCherney@proton.me>
2023-07-15 23:50:51 -0500

    made shell for other functions

--- bitburner/BitNode10/sleeve.js
+++ bitburner/BitNode10/sleeve.js
@@ -1,39 +1,58 @@
 /** @param {NS} ns */
 export async function main(ns) {
+	
+	// Debug
 
-
-	//ns.tail();
+	ns.tail();
 	ns.clearLog();
+
 	var sleeves = ns.sleeve.getNumSleeves();
 
+	await sleeveInitialize(ns, sleeves);
 
-	while (true) {
+
+
+}
+
+
+
+
+
+// reduces shock to 0 and increases sync to 100
+
+export function sleeveInitialize(ns, sleeves) {
+
+	// sets loop
+	var loop = true;
+
+	while (loop) {
 
 		var breakpoint = 0;
 
 		for (let i=0; i < sleeves; i++) {
 
+			// getSleeve returns json
 			var jsonSleeve = ns.sleeve.getSleeve(i);
 			var shock = jsonSleeve.shock;
 			var sync = jsonSleeve.sync;
 			
-			if (shock > 0) {
-				ns.sleeve.setToShockRecovery(i);
-			} else if (sync < 100) {
-				ns.sleeve.setToSynchronize(i);
+
+			if (sync < 100) {
+				ns.sleeve.setToSynchronize(i); 
+			} else if (shock > 0) {
+				ns.sleeve.setToShockRecovery(i); 
 			} else {
 				breakpoint++;
 				ns.print(breakpoint);
 			}
-			
-			
+		}
+	
+		if (breakpoint === sleeves) {
+			loop = false;
+		} else {
+			ns.sleep('10000');
 		}
 
-		if (breakpoint === sleeves) {
-			ns.kill('sleeve.js');
-		} else {
-			await ns.sleep('100000');
-		}
 
 	}
 }
