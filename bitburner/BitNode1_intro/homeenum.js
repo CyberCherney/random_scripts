e75047bdcbc356711d534a316a0de2fe38f77b75
CyberCherney <CyberSecCherney@proton.me>
2023-07-23 17:53:51 -0500

    added check for running scripts

--- bitburner/BitNode1_intro/homeenum.js
+++ bitburner/BitNode1_intro/homeenum.js
@@ -3,23 +3,26 @@
 import { scan } from "basic.js";
 
 export async function main(ns) {
-	ns.tail();
+	//ns.tail();
 	const servers = scan(ns);
 
 	var ram = ns.getServerMaxRam('home');
 	var neededRam = ns.getScriptRam('homehack.js');
-	var threads = Math.floor((ram - 25)/neededRam); 
+	var threads = Math.floor((ram - 70)/neededRam);
+	if (threads < 0 || ns.scriptRunning("homehack.js", ns.getHostname())) {
+		ns.kill('homeenum.js');
+	} 
 	for (let i = 0; i < servers.length; i++) {
 		if (ns.hasRootAccess(servers[i])) {
 			var cash = ns.getServerMoneyAvailable(servers[i]);
 			var max = ns.getServerMaxMoney(servers[i]);
 			if ((cash/max) > .3 && servers[i] != ns.read('target.txt')) {
-				await ns.run('homehack.js', threads, servers[i]);
-				break
+				ns.run('homehack.js', threads, servers[i]);
 			} else {
 				var file = ns.read('target.txt');
 				var json = JSON.parse(file);
-				await ns.run('homehack.js', threads, json.host);
+				await ns.sleep(10);
+				ns.run('homehack.js', threads, json.host);
 				break
 			}
 		}
