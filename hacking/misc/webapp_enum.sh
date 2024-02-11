96eec711bbcc1436ebf7138ffecb1d2c118aa0ea
CyberCherney <CyberSecCherney@proton.me>
2024-02-11 14:57:06 -0600

    added gowitness subjack and waybackurl to recon script

--- hacking/misc/webapp_enum.sh
+++ hacking/misc/webapp_enum.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# tools needed for this script:
-# assetfinder, amass*, httprobe, gowitness, google chrome in $PATH
 # simple subdomain scanner, filtering, and screenshotting script
+# tools needed for this script:
+# assetfinder, amass*, httprobe, gowitness, google chrome in $PATH, subjack, waybackurls
+#
+# some of the above tools recommend using go get, instead use the following command format:
+# go install github.com/haccer/subjack@latest
 
 
 url=$1
@@ -32,16 +35,75 @@ if [ ! -f "$root/httprobe/alive.txt" ];then
 fi
 
 echo "[+] Finding subdomains with assetfinder"
-assetfinder $url >> $url/recon/assets.txt
-cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
-rm $url/recon/assets.txt
+assetfinder $url >> $root/assets.txt
+cat $root/assets.txt | sort -u | grep $1 >> $root/final.txt
+rm $root/assets.txt
 
 : '
 echo "[+] Finding subdomains with amass"
-amass enum -d $url >> $url/recon/f.txt
-sort -u $url/recon/f.txt >> $url/recon/final.txt
-rm $url/recon/f.txt
+amass enum -d $url >> $root/f.txt
+sort -u $root/f.txt >> $root/final.txt
+rm $root/f.txt
 '
 
 echo "[+] Probing for live endpoints with httprobe"
-cat $url/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/alive.txt
+cat $root/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $root/httprobe/a.txt
+sort -u $root/httprobe/a.txt > $root/httprobe/alive.txt
+rm $root/httprobe/a.txt
+
+echo "[+] Checking for subdomain takeovers with subjack"
+
+if [ ! -f "$root/potential_takeovers/potential_takeovers.txt" ];then
+	touch $root/potential_takeovers/potential_takeovers.txt
+fi
+
+subjack -w $root/final.txt -t 100 -timeout 30 -ssl -c ~/go/pkg/mod/github.com/haccer/subjack@v0.0.0-20201112041112-49c51e57deab/fingerprints.json -v 3 -o $root/potential_takeovers/potential_takeovers.txt
+
+echo "[+] Scanning for ports with nmap"
+nmap -iL $root/httprobe/alive.txt -T4 -oA $root/scans/scanned.txt
+
+echo "[+] Scraping the wayback with waybackurls"
+cat $root/final.txt | waybackurls >> $root/wayback/wayback_output.txt
+sort -u $root/wayback/wayback_output.txt
+
+echo "[+] Compiling extentions "
+cat $root/wayback/wayback_output.txt | grep '?*=' | cut -d '=' -f 1 | sort -u >> $root/wayback/params/wayback_params.txt
+for line in $(cat $root/wayback/params/wayback_params.txt);do echo $line'=';done
+
+echo "[+] Pulling and compiling js/php/aspx/jsp/json files from wayback output..."
+for line in $(cat $root/wayback/wayback_output.txt);do
+	ext="${line##*.}"
+	if [[ "$ext" == "js" ]]; then
+		echo $line >> $root/wayback/extensions/js1.txt
+		sort -u $root/wayback/extensions/js1.txt >> $root/wayback/extensions/js.txt
+	fi
+	if [[ "$ext" == "html" ]];then
+		echo $line >> $root/wayback/extensions/jsp1.txt
+		sort -u $root/wayback/extensions/jsp1.txt >> $root/wayback/extensions/jsp.txt
+	fi
+	if [[ "$ext" == "json" ]];then
+		echo $line >> $root/wayback/extensions/json1.txt
+		sort -u $root/wayback/extensions/json1.txt >> $root/wayback/extensions/json.txt
+	fi
+	if [[ "$ext" == "php" ]];then
+		echo $line >> $root/wayback/extensions/php1.txt
+		sort -u $root/wayback/extensions/php1.txt >> $root/wayback/extensions/php.txt
+	fi
+	if [[ "$ext" == "aspx" ]];then
+		echo $line >> $root/wayback/extensions/aspx1.txt
+		sort -u $root/wayback/extensions/aspx1.txt >> $root/wayback/extensions/aspx.txt
+	fi
+done
+ 
+rm $root/wayback/extensions/js1.txt
+rm $root/wayback/extensions/jsp1.txt
+rm $root/wayback/extensions/json1.txt
+rm $root/wayback/extensions/php1.txt
+rm $root/wayback/extensions/aspx1.txt
+
+echo "[+] Screencapping compiled domains with gowitness"
+gowitness file -f $root/final.txt
+mv screenshots $root/gowitness
+mv gowitness.sqlite3 $root/gowitness
+
+
