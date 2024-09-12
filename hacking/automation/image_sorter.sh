3f8ef84ea095e3d7042d7ca58f5951619a0f0af9
CyberCherney <CyberSecCherney@proton.me>
2024-09-12 11:10:57 -0500

    added comments + more cleanup

--- hacking/automation/image_sorter.sh
+++ hacking/automation/image_sorter.sh
@@ -19,7 +19,7 @@ for line in `ls | grep "$box""_"`; do
 done
 
 i=1
-IFS=$'\t\n'
+IFS=$'\t\n' # makes the line search for new line instead of the default space
 for line in $(cat $directory/to_be_sorted | sort -u); do
     img=`echo "$line" | cut -b 37-`
     stripped=`echo $img | sed "s/$$prefix//g"`
@@ -29,9 +29,18 @@ for line in $(cat $directory/to_be_sorted | sort -u); do
 done
 
 rm $directory/to_be_sorted
+
+
+# this part is specifically for my environment 
+# remove or modify if you intend to use
+
 cd $directory
 
 python3 -m http.server 8081
 echo "Server is up, run the following command:"
-read -p "wget -r http://192.168.2.69:8081/" 
+echo "wget -r http://192.168.2.69:8081/"
+echo "" 
+read -p "Hit Enter when done for cleanup."
 
+cd ..
+mv $directory transfer
